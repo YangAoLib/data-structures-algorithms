578c1cbce09074547e668b260def88528df9acf4
YangAo <ya1398789131@163.com>
2023-10-16 23:23:34 +0800

    数组实现栈

---
+++ src/main/java/edu/yangao/stack/ArrayStack.java
@@ -0,0 +1,71 @@
+package edu.yangao.stack;
+
+import java.util.Scanner;
+
+/**
+ * 用数据来模拟栈
+ */
+public class ArrayStack implements Stack<Integer> {
+
+    public static void main(String[] args) {
+        new ArrayStack(3).test();
+    }
+
+    public ArrayStack(Integer maxSize) {
+        this.maxSize = maxSize;
+        this.top = -1;
+        this.array = new Integer[maxSize];
+    }
+
+    @Override
+    public Boolean isFull() {
+        return this.top == this.maxSize - 1;
+    }
+
+    @Override
+    public Boolean isEmpty() {
+        return this.top == -1;
+    }
+
+    @Override
+    public void push(Integer value) {
+        if (isFull()) throw new RuntimeException("栈已满");
+        this.array[++this.top] = value;
+    }
+
+    @Override
+    public Integer pop() {
+        if (isEmpty()) throw new RuntimeException("栈已空");
+        return this.array[this.top--];
+    }
+
+    @Override
+    public void list() {
+        System.out.print("栈中元素: ");
+        for (int i = top; i > -1; --i) {
+            System.out.print(this.array[i]);
+            if (i != 0) System.out.print(", ");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Integer getValueFromScanner(Scanner scanner) {
+        return scanner.nextInt();
+    }
+
+    /**
+     * 栈顶指向
+     */
+    private Integer top;
+
+    /**
+     * 数组大小(最多可以存储数据的个数)
+     */
+    private Integer maxSize;
+
+    /**
+     * 存储数据的数组
+     */
+    private Integer[] array;
+}
---
+++ src/main/java/edu/yangao/stack/Stack.java
@@ -0,0 +1,87 @@
+package edu.yangao.stack;
+
+import java.util.Scanner;
+
+/**
+ * 栈
+ */
+public interface Stack<T> {
+
+    /**
+     * 当前栈是否满
+     * @return 当前栈是否满
+     */
+    Boolean isFull();
+
+    /**
+     * 当前栈是否为空
+     * @return 当前栈是否为空
+     */
+    Boolean isEmpty();
+
+    /**
+     * 入栈
+     * @param value 存入值
+     */
+    void push(T value);
+
+    /**
+     * 出栈
+     * @return 出栈元素
+     */
+    T pop();
+
+    /**
+     * 遍历输出内容
+     */
+    void list();
+
+    /**
+     * 从输入获取值
+     * @param scanner 输入
+     * @return 获取的值
+     */
+    T getValueFromScanner(Scanner scanner);
+
+    /**
+     * 测试方法
+     */
+    default void test() {
+        Scanner scanner = new Scanner(System.in);
+        out:
+        while (true) {
+            System.out.println("show: 遍历显示栈");
+            System.out.println("exit: 退出程序");
+            System.out.println("push: 入栈");
+            System.out.println("pop: 出栈");
+            System.out.print("请输入你的选择: ");
+            String key = scanner.next();
+            switch (key) {
+                case "show":
+                    this.list();
+                    break;
+                case "push":
+                    System.out.print("请输入入栈值: ");
+                    T value = getValueFromScanner(scanner);
+                    try {
+                        this.push(value);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "pop":
+                    try {
+                        System.out.println("出栈的数据是: " + this.pop());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "exit":
+                    scanner.close();
+                    break out;
+                default:
+                    System.out.println("输入错误请重新输入~~~");
+            }
+        }
+    }
+}
